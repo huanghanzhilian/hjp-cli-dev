6fae5290ba9b8d793ae29d75683c6661fa18199f
huangjipeng <1319639755@qq.com>
2022-03-07 22:14:59 +0800

    feat: 最低node版本号功能开发

---
+++ core/cli/lib/const.js
@@ -0,0 +1,6 @@
+const NODE_LOW_VERSION = '12.0.0';
+// const NODE_LOW_VERSION = '14.18.2';
+
+module.exports = {
+  NODE_LOW_VERSION
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,9 +1,29 @@
-const pkg = require('../../../package.json');
+const semver = require('semver');  // 对比版本号
+const colors = require("colors");  // 对输入的log染色
 
+const pkg = require('../../../package.json');
 const log = require('@hjp-cli-dev/log');
 
+const {NODE_LOW_VERSION} = require('./const');
+
 const core = function () {
-  log.notice('cli', pkg.version)
+  try {
+    checkVersion();
+    checkLowNodeVersion();
+  } catch (e) {
+    log.error(e.message)
+  }
+}
+
+const checkVersion = function () {
+  log.info('cli', pkg.version)
+}
+
+const checkLowNodeVersion = function () {
+  let current = process.version;
+  if (!semver.gte(current, NODE_LOW_VERSION)) {
+    throw new Error(colors.red(`不符合最低Node版本要求，要求最低${NODE_LOW_VERSION}`))
+  }
 }
 
 module.exports = core
