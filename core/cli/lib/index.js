ea781a9ba8529eb4e9d2d102a0d6e41a3174e16b
huangjipeng <1319639755@qq.com>
2022-03-07 23:00:24 +0800

    feat: 检查用户主目录

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,5 +1,7 @@
 const semver = require('semver');  // 对比版本号
 const colors = require("colors");  // 对输入的log染色
+const os = require('os');
+const pathExists = require('path-exists').sync; // 判断目标文件或文件夹是否存在
 
 const pkg = require('../../../package.json');
 const log = require('@hjp-cli-dev/log');
@@ -11,15 +13,23 @@ const core = function () {
     checkVersion();
     checkLowNodeVersion();
     checkRoot()
+    checkUserHome()
   } catch (e) {
     log.error(e.message)
+  }
+}
+
+const checkUserHome = () => {
+  const userHome = os.homedir();
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('用户目录不存在！'))
   }
 }
 
 const checkRoot = function () {
   const rootCheck = require('root-check');  // root降级，使用普通用户启动脚手架
   rootCheck()
-  console.log(process.getuid())
+  // console.log(process.getuid())
 }
 
 const checkVersion = function () {
