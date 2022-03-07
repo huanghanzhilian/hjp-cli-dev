b0271d833954222a3785a9d36766046b555b501b
huangjipeng <1319639755@qq.com>
2022-03-07 23:14:57 +0800

    feat: 检查入参

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,21 +2,40 @@ const semver = require('semver');  // 对比版本号
 const colors = require("colors");  // 对输入的log染色
 const os = require('os');
 const pathExists = require('path-exists').sync; // 判断目标文件或文件夹是否存在
+const minimist = require("minimist"); // 解析参数
 
 const pkg = require('../../../package.json');
 const log = require('@hjp-cli-dev/log');
 
 const {NODE_LOW_VERSION} = require('./const');
 
+let args = null;
+
 const core = function () {
   try {
-    checkVersion();
-    checkLowNodeVersion();
+    checkVersion()
+    checkLowNodeVersion()
     checkRoot()
     checkUserHome()
+    checkInputArgs()
+    log.verbose('debug', 'test debug log')
   } catch (e) {
     log.error(e.message)
   }
+}
+
+const checkInputArgs = function () {
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+const checkArgs = function () {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL;
 }
 
 const checkUserHome = () => {
