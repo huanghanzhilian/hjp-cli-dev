b4af08097ecea58dc16c5a93a415f50d05f44d8d
huangjipeng <1319639755@qq.com>
2022-03-21 17:24:21 +0800

    feat: 强制清空当前目录功能开发

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const fs = require('fs')
-
 const Command = require("@hjp-cli-dev/command")
+const inquirer = require('inquirer');
+const log = require('@hjp-cli-dev/log')
+const fse = require('fs-extra')
 
 function init(programName, options, commandObj) {
   const cmd = new InitCommand(programName, options, commandObj)
@@ -14,28 +16,47 @@ class InitCommand extends Command {
     if (!this.args.length) throw new Error('argument of init command is required')
     this.projectName = arguments[0]
     this.force = this.args[1].force
+    this.runPath = process.cwd()
   }
 
-  exec() {
-    // 前险
-    this.prepare()
-    // 下载内容
+  async exec() {
+    try {
+      await this.prepare()
+      // 下载内容
+    } catch (e) {
+      log.error('error in init command execution: ', e)
+    }
   }
 
-  prepare() {
-    // Check whether the directory is empty
-    if(!this.isDirEmpty()){
-      // Asks whether clearing the current directory
+  async prepare() {
+    // 不为空
+    if (!this.isDirEmpty()) {
+      let isClearDir
+      if (!this.force) {
+        // Asks whether clearing the current directory
+        const {clearDir} = await inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'clearDir',
+            default: false,
+            message: 'The current directory is not empty.Do you want to clear the current directory ?'
+          }
+        ])
+        isClearDir = clearDir
+      }
+
+      if (isClearDir || this.force) {
+        await fse.emptydir(this.runPath)
+      }
     }
   }
 
   isDirEmpty() {
-    let files = fs.readdirSync(process.cwd())
+    let files = fs.readdirSync(this.runPath)
     files = files.filter(fileName =>
       // fileName filter
       !fileName.startsWith('.') && !'node_modules'.includes(fileName)
     )
-    console.log(files.length === 0)
     return files.length === 0
   }
 }
